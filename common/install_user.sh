57a3fdb6ac228ec176b03d6aeae0906b8fb71c9f
Edward Z. Yang <ezyang@fb.com>
2017-10-30 17:21:47 -0400

    Chown Jenkins homedir to jenkins
    Signed-off-by: Edward Z. Yang <ezyang@fb.com>

--- common/install_user.sh
+++ common/install_user.sh
@@ -8,6 +8,7 @@ echo "jenkins:x:1001:" >> /etc/group
 
 # Create $HOME
 mkdir -p /var/lib/jenkins
+chown jenkins:jenkins /var/lib/jenkins
 
 # Allow writing to /usr/local (for make install)
 chown jenkins:jenkins /usr/local
