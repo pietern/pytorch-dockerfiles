03a97c71c3a756f158d649bda27afca7b7a334c0
Edward Z. Yang <ezyang@fb.com>
2017-12-22 14:51:02 -0500

    Install pytest, scipy and scikit-image on Conda Python.
    Signed-off-by: Edward Z. Yang <ezyang@fb.com>

--- common/install_conda.sh
+++ common/install_conda.sh
@@ -45,6 +45,9 @@ if [ -n "$CONDA_VERSION" ]; then
   elif [[ "$BUILD" == *cuda9-cudnn7* ]]; then
     sudo -H -u jenkins /opt/conda/bin/conda install -q -y magma-cuda90 -c soumith
   fi
+
+  # Install some other packages
+  sudo -u jenkins /opt/conda/bin/pip install -y pytest scipy==0.19.1 scikit-image
 fi
 
 # Cleanup package manager
