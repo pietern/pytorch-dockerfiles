2f8692f0b59ba8433de6b4aa554116f0f62dd991
Edward Z. Yang <ezyang@fb.com>
2018-03-10 22:41:57 -0500

    Need pip somehow.
    Signed-off-by: Edward Z. Yang <ezyang@fb.com>

--- common/install_travis_python.sh
+++ common/install_travis_python.sh
@@ -26,6 +26,19 @@ if [ -n "$TRAVIS_PYTHON_VERSION" ]; then
   apt-get update
   apt-get install -y gfortran
 
+  # Install pip from source.
+  # The python-pip package on Ubuntu Trusty is old
+  # and upon install numpy doesn't use the binary
+  # distribution, and fails to compile it from source.
+  pushd tmp
+  as_jenkins curl -O https://pypi.python.org/packages/11/b6/abcb525026a4be042b486df43905d6893fb04f05aac21c32c638e939e447/pip-9.0.1.tar.gz
+  as_jenkins tar zxf pip-9.0.1.tar.gz
+  pushd pip-9.0.1
+  as_jenkins python setup.py install
+  popd
+  rm -rf pip-9.0.1*
+  popd
+
   # Install pip packages
   as_jenkins pip install --upgrade pip
 
