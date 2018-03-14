5a8c1e334be6747c7ae14156c232a160ebb01ffc
Edward Z. Yang <ezyang@fb.com>
2018-03-13 22:21:53 -0400

    Expunge legacy BUILD envvar
    Signed-off-by: Edward Z. Yang <ezyang@fb.com>

--- common/install_user.sh
+++ common/install_user.sh
@@ -2,26 +2,6 @@
 
 set -ex
 
-if [[ "$BUILD" == *py2.7* ]]; then
-  export PYTHON_VERSION=2.7
-fi
-
-if [[ "$BUILD" == *py2.7.9* ]]; then
-  export PYTHON_VERSION=2.7.9
-fi
-
-if [[ "$BUILD" == *py3.5* ]]; then
-  export PYTHON_VERSION=3.5
-fi
-
-if [[ "$BUILD" == *py3.6* ]]; then
-  export PYTHON_VERSION=3.6
-fi
-
-if [[ "$BUILD" == *pynightly* ]]; then
-  export PYTHON_VERSION=nightly
-fi
-
 # Mirror jenkins user in container
 echo "jenkins:x:1014:1014::/var/lib/jenkins:" >> /etc/passwd
 echo "jenkins:x:1014:" >> /etc/group
