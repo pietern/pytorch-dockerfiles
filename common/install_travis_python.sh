37ebe3d3ea60e0479ffa6f639e47ab720ee121ce
Edward Z. Yang <ezyang@fb.com>
2018-05-04 17:40:58 -0400

    Follow redirects in curl
    Signed-off-by: Edward Z. Yang <ezyang@fb.com>

--- common/install_travis_python.sh
+++ common/install_travis_python.sh
@@ -36,7 +36,7 @@ if [ -n "$TRAVIS_PYTHON_VERSION" ]; then
   # and upon install numpy doesn't use the binary
   # distribution, and fails to compile it from source.
   pushd tmp
-  as_jenkins curl -O https://pypi.python.org/packages/11/b6/abcb525026a4be042b486df43905d6893fb04f05aac21c32c638e939e447/pip-9.0.1.tar.gz
+  as_jenkins curl -L -O https://pypi.python.org/packages/11/b6/abcb525026a4be042b486df43905d6893fb04f05aac21c32c638e939e447/pip-9.0.1.tar.gz
   as_jenkins tar zxf pip-9.0.1.tar.gz
   pushd pip-9.0.1
   as_jenkins python setup.py install
