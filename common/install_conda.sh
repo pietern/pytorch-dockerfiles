f99214be38011ea51899e51b6f253c305d0a3af5
Tongzhou Wang <SsnL@users.noreply.github.com>
2018-08-30 11:16:56 -0400

    Bump librosa to >= 0.6.2

--- common/install_conda.sh
+++ common/install_conda.sh
@@ -63,5 +63,5 @@ if [ -n "$ANACONDA_VERSION" ]; then
 
   # Install some other packages
   # TODO: Why is scipy pinned
-  as_jenkins pip install -q pytest scipy==1.1.0 scikit-image librosa
+  as_jenkins pip install -q pytest scipy==1.1.0 scikit-image librosa>=0.6.2
 fi
