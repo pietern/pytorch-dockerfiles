84c4187f4290092876016bdb6f0cb738143b8975
Edward Z. Yang <ezyang@fb.com>
2018-10-28 22:11:12 -0400

    Install dev and deps
    Signed-off-by: Edward Z. Yang <ezyang@fb.com>

--- common/install_deadsnakes_python.sh
+++ common/install_deadsnakes_python.sh
@@ -6,9 +6,18 @@ if [ -n "$DEADSNAKES_PYTHON_VERSION" ]; then
 
   add-apt-repository ppa:deadsnakes/ppa
   apt-get update
-  apt-get install -y python$DEADSNAKES_PYTHON_VERSION
+  apt-get install -y python$DEADSNAKES_PYTHON_VERSION python$DEADSNAKES_PYTHON_VERSION-dev
 
   update-alternatives --install /usr/bin/python python /usr/bin/python"$DEADSNAKES_PYTHON_VERSION" 50
+
+  # Bootstrap pip
+  curl https://bootstrap.pypa.io/get-pip.py | python
+
+  # Install basic dependencies
+  pip install numpy pyyaml future hypothesis protobuf pytest pillow typing mkl mkl-devel
+  if [[ "$DEADSNAKES_PYTHON_VERSION" != "3.7" ]]; then
+    pip install scipy==1.1.0 scikit-image
+  fi
 
   # Cleanup package manager
   apt-get autoclean && apt-get clean
