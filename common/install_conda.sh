f71f24db38dfda69307797cca077852724f33115
Edward Z. Yang <ezyang@fb.com>
2019-07-25 15:15:47 -0700

    Pin Numba due to ASAN uncleanness in Numba
    Signed-off-by: Edward Z. Yang <ezyang@fb.com>

--- common/install_conda.sh
+++ common/install_conda.sh
@@ -82,7 +82,8 @@ if [ -n "$ANACONDA_PYTHON_VERSION" ]; then
 
   # Install some other packages
   # TODO: Why is scipy pinned
-  as_jenkins pip install -q pytest scipy==1.1.0 scikit-image librosa>=0.6.2 psutil
+  # numba is pinned because of https://github.com/numba/numba/issues/4368
+  as_jenkins pip install -q pytest scipy==1.1.0 scikit-image librosa>=0.6.2 psutil numba==0.43.0
 
   popd
 fi
