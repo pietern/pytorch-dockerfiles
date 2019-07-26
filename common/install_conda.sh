2986f432bb937f5aa4cc149e20b1f1fa0e49e742
Edward Z. Yang <ezyang@fb.com>
2019-07-26 11:28:20 -0700

    Pin scikit-learn too
    Signed-off-by: Edward Z. Yang <ezyang@fb.com>

--- common/install_conda.sh
+++ common/install_conda.sh
@@ -83,7 +83,10 @@ if [ -n "$ANACONDA_PYTHON_VERSION" ]; then
   # Install some other packages
   # TODO: Why is scipy pinned
   # numba & llvmlite is pinned because of https://github.com/numba/numba/issues/4368
-  as_jenkins pip install --progress-bar off pytest scipy==1.1.0 scikit-image librosa>=0.6.2 psutil numba==0.43.1 llvmlite==0.28.0
+  # scikit-learn is pinned because of
+  # https://github.com/scikit-learn/scikit-learn/issues/14485 (affects gcc 5.5
+  # only)
+  as_jenkins pip install --progress-bar off pytest scipy==1.1.0 scikit-learn==0.20.3 scikit-image librosa>=0.6.2 psutil numba==0.43.1 llvmlite==0.28.0
 
   popd
 fi
